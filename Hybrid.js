03e26202ce8c5a0adcff3729834522718983ee8d
yizhi.li <tgxpuisb@hotmail.com>
2017-09-02 22:17:12 +0800

    [update] abstract lib

--- Hybrid.js
+++ Hybrid.js
@@ -1,31 +1,11 @@
-import bridge from './bridge'
-
-function genFeatureFunc (feature) {
-    return (...args) => {
-        let data = args.shift()
-        let callback = args.shift()
-        if (!data) {
-            data = {}
-        } else if (typeof data === 'function') {
-            callback = data
-            data = {}
-        }
-        if (typeof callback !== 'function') {
-            callback = () => {}
-        }
-
-        bridge.send({
-            data,
-            target: feature
-        }, (resp) => {
-            if (resp.error) {
-                callback(resp.error)
-            } else {
-                callback(null, resp.result)
-            }
-        })
-    }
-}
+/**
+ * 对bridge调用进行的封装
+ * 一个完整的hybrid请求是如下:
+ * 前端向客户端发送target:'config'的信息期间带上注册信息,客户端收到注册信息后向前端返回一个列表,列表是一个注册成功的方法数组.
+ * 此时前端再向客户端发送target:'hybrid-method'方法,此时客户端真正了解前端的意图,开始进行一系列客户端操作,并且操作结束后调用前端挂载在window上的函数给前端回调
+ * 基于上面的操作,如果直接使用bridge你将会面临2到3层的回调,这种写法在业务代码中很不优雅,所以我们将hybrid的异步进行了封装,希望能够简化这一操作流程
+ */
+import core from './lib/core'
 
 function Hybrid (apiList) {
     if (Hybrid.instance) {
@@ -34,13 +14,15 @@ function Hybrid (apiList) {
         return new Hybrid(apiList)
     }
     Hybrid.instance = this
-    this.hasPreConfigApis = []
-    this.hybridMethods = {}
+    this.hasPreConfigApis = [] // 已经注册过的api, 防止重复注册
+    this.hybridMethods = core
     this.handleApiList(apiList)
 }
 
+Hybrid.__version__ = '0.1.0'
+
 Hybrid.prototype.handleApiList = function handleApiList (apiList) {
-    let list
+    let list = []
     if (Array.isArray(apiList)) {
         list = apiList
     } else if (typeof apiList === 'string') {
@@ -49,64 +31,40 @@ Hybrid.prototype.handleApiList = function handleApiList (apiList) {
         list = Object.keys(apiList)
     }
     for (let i = 0; i < list.length; i += 1) {
-        this.preConfig(list[i])
+        this.config(list[i])
     }
-
     return this
 }
 
-Hybrid.prototype.preConfig = function preConfig (feature) {
-    if (this.hasPreConfigApis.indexOf(feature) === -1) {
+Hybrid.prototype.config = function config (feature) {
+    if (this.hasPreConfigApis.indexOf(feature) === -1 /* 防止重复注册 */) {
+        // 在原型上面挂载注册的方法,方便直接调用
         /* eslint-disable func-names */
         Hybrid.prototype[feature] = function (...args) {
-            if (bridge) {
-                if (typeof this.hybridMethods[feature] === 'function') {
-                    this.hybridMethods[feature](...args)
-                } else {
-                    this.config({
-                        jsApiList: {
-                            feature: 'optional'
-                        }
-                    }, (err, result) => {
-                        if (!err && result.length > 0) {
-                            this.hybridMethods[feature](...args)
-                        }
-                    })
-                }
+        /* eslint-enable */
+            if (typeof core[feature] === 'function') {
+                core[feature](...args)
+            } else {
+                // 对没有注册的hybrid方法进行注册
+                core.config({
+                    jsApiList: {
+                        [feature]: 'optional'
+                    }
+                }, (err, result) => {
+                    if (!err && result.length > 0) {
+                        core[feature](...args)
+                    } else {
+                        /* eslint-disable no-console */
+                        console.error(`can not find method ${feature},you can check hybird-api list`)
+                        /* eslint-enable */
+                    }
+                })
             }
             return this
         }
     }
     return this
 }
-
-Object.defineProperty(Hybrid.prototype, 'config', {
-    value (config, onReady) {
-        if (!bridge) {
-            return this
-        }
-        bridge.send({
-            data: config,
-            target: 'config'
-        }, (response) => {
-            if (response.error) {
-                onReady(response.error)
-            } else if (response.result) {
-                const featureList = response.result
-                for (let i = 0; i < featureList.length; i += 1) {
-                    const feature = featureList[i]
-                    if (typeof this.hybridMethods[feature] !== 'function') {
-                        this.hybridMethods[feature] = genFeatureFunc(feature)
-                    }
-                    onReady(null, response.result)
-                }
-            }
-        })
-        return this
-    },
-    writable: false,
-    configurable: false
-})
 
 Hybrid.instance = null
 
@@ -117,7 +75,9 @@ Hybrid.getInstance = function getInstance () {
     return new Hybrid()
 }
 
+Hybrid.prototype.use = function use () {
+
+}
+
 
 export default Hybrid
-
-
